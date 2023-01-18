d0dd826c682640ebe9585a383933b37840fc87d9
austin <austin60616@gmail.com>
2023-01-18 19:12:31 +0800

    finished temperature meter

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [temperature, setTemperature] = useState(20);
+  const [color, setColor] = useState("");
+
+  useEffect(() => {
+    if (temperature >= 28) {
+      setColor("hot");
+    } else if (temperature < 28 && temperature >= 15) {
+      setColor("");
+    } else {
+      setColor("cold");
+    }
+  }, [temperature]);
+  function increaseTemperature() {
+    setTemperature(temperature + 1);
+  }
+  function decreaseTemperature() {
+    setTemperature(temperature - 1);
+  }
+
   return (
     <div className="container">
       <div className="rectangle">
         <div className="top">
-          <div className="temperature">10°C</div>
+          <div className={`temperature ${color}`}>{temperature}°C</div>
         </div>
         <div className="bottom">
-          <div className="circle">-</div>
-          <div className="circle">+</div>
+          <div className="circle" onClick={decreaseTemperature}>
+            -
+          </div>
+          <div className="circle" onClick={increaseTemperature}>
+            +
+          </div>
         </div>
       </div>
     </div>
